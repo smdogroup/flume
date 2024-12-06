eb7afe85e56c52b9e9e017cb0242f54dd0f71542
csmith763 <csmith763@gatech.edu>
2024-12-06 15:12:04 -0500

    Testing pre-commit

--- tests/test_state.py
+++ tests/test_state.py
@@ -2,6 +2,7 @@ import numpy as np
 from base_classes.state import State
 import unittest
 
+
 class TestStateConstruction(unittest.TestCase):
     """
     Tests the construction of a State object from the framework.
@@ -27,7 +28,7 @@ class TestStateConstruction(unittest.TestCase):
         self.assertEqual(2.0, self.scalar_state.value, "Values do not match")
 
         return
-    
+
     def test_scalar_desc(self):
         """
         Checks the description of the scalar state
@@ -46,16 +47,18 @@ class TestStateConstruction(unittest.TestCase):
         self.assertEqual(self.scalar_state.data_type, float, "Types do not match")
 
         return
-    
+
     def test_vec_value(self):
         """
         Checks the value of the vector state, which uses numpy
         """
 
         # self.assertEqual(np.array([1.0, 2.0, 3.0]), self.vec_state.value, "Values do not match")
-        np.testing.assert_array_equal(np.array([1.0, 2.0, 3.0]), self.vec_state.value, "Values do not match")
+        np.testing.assert_array_equal(
+            np.array([1.0, 2.0, 3.0]), self.vec_state.value, "Values do not match"
+        )
 
-        return 
+        return
 
     def test_vec_desc(self):
         """
@@ -66,7 +69,7 @@ class TestStateConstruction(unittest.TestCase):
         self.assertEqual(test_str, self.vec_state.desc, "Descriptions do not match")
 
         return
-    
+
     def test_vec_type(self):
         """
         Checks the type of the vector state
@@ -75,13 +78,14 @@ class TestStateConstruction(unittest.TestCase):
         self.assertEqual(self.vec_state.data_type, np.ndarray, "Types do not match")
 
         return
-    
+
     def test_vec_shape(self):
         """
         Checks the shape of the vector state
         """
 
         self.assertEqual(self.vec_state.shape, (3,), "Shapes do not match")
+
 
 if __name__ == "__main__":
     # Run the unittests
