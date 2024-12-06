6da42e2d992544405a2b93ce1825ac33d23d67bd
csmith763 <csmith763@gatech.edu>
2024-12-06 16:12:35 -0500

    Format update part2

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -3,6 +3,7 @@ from base_classes.analysis import Analysis
 from base_classes.state import State
 import unittest
 
+
 class FunctionAnalysis(Analysis):
     """
     Analysis class that computes the scalar value of an analytic function. This class is used for testing purposes.
@@ -25,12 +26,12 @@ class FunctionAnalysis(Analysis):
         -----------------
         a : float
             First parameter for the analytic function
-        b : float 
+        b : float
             Second parameter for the analytic function
         """
-        
+
         # Set the default parameters for the object
-        self.default_parameters = {"a":1.0, "b":2.0}
+        self.default_parameters = {"a": 1.0, "b": 2.0}
 
         # Perform the base class object initialization
         super().__init__(obj_name=obj_name, sub_analyses=sub_analyses, **kwargs)
@@ -41,10 +42,10 @@ class FunctionAnalysis(Analysis):
         y_var = State(value=1.0, desc="y variable input", source=self)
 
         # Construct the variables dictionary
-        self.variables = {"x":x_var, "y":y_var}
+        self.variables = {"x": x_var, "y": y_var}
 
         return
-    
+
     def _analyze(self):
         """
         Private analysis method for the object that computes all of the outputs with the provided input variables and parameters to the object
@@ -68,7 +69,7 @@ class FunctionAnalysis(Analysis):
         self.outputs["f"] = State(value=self.f, desc="value of the output", source=self)
 
         return
-    
+
     def _analyze_adjoint(self):
         """
         Private adjoint analysis method for the object that computes the derivative values for the input variables via the adjoint method.
@@ -99,6 +100,7 @@ class FunctionAnalysis(Analysis):
         # Update the attribute for the adjoint analysis
         self.adjoint_analyzed = True
 
+
 class TestAnalysisObject(unittest.TestCase):
     """
     Tests the implementation of an Analysis object for a simple analytic function.
@@ -118,8 +120,8 @@ class TestAnalysisObject(unittest.TestCase):
 
         self.test_obj = FunctionAnalysis(obj_name="test", a=self.a, b=self.b)
 
-        return 
-    
+        return
+
     def test_parameter_values(self):
         """
         Checks that the stored parameter values match those randomly computed
@@ -134,21 +136,25 @@ class TestAnalysisObject(unittest.TestCase):
         # Loop throuch each parameter and perform the check
         for i, name in enumerate(param_names):
             with self.subTest(f"parameter = {name}"):
-                self.assertEqual(param_vals[i], self.test_obj.parameters[name], f"Values for parameter {name} do not match")
+                self.assertEqual(
+                    param_vals[i],
+                    self.test_obj.parameters[name],
+                    f"Values for parameter {name} do not match",
+                )
 
         return
-    
+
     def test_set_var_values(self):
         """
         Checks the functionality of the set variable values method
         """
 
         # Set the variable values for the analysis object
-        self.test_obj.set_var_values(variables={"x":self.x, "y":self.y})
+        self.test_obj.set_var_values(variables={"x": self.x, "y": self.y})
 
         # Store the known variable values
         var_names = ["x", "y"]
-        known_vals = {"x":self.x, "y":self.y}
+        known_vals = {"x": self.x, "y": self.y}
 
         # Extract the variable values
         var_vals = self.test_obj.get_var_values()
@@ -156,17 +162,21 @@ class TestAnalysisObject(unittest.TestCase):
         # Loop through each varaible and perform the check
         for i, name in enumerate(var_names):
             with self.subTest(f"variable = {name}"):
-                self.assertEqual(known_vals[name], var_vals[name], f"Values for variable {name} do not match")
+                self.assertEqual(
+                    known_vals[name],
+                    var_vals[name],
+                    f"Values for variable {name} do not match",
+                )
 
         return
-    
+
     def test_output_values(self):
         """
         Checks the computation of the output value for the analytic function
         """
 
         # Set the variable values for the analysis object
-        self.test_obj.set_var_values(variables={"x":self.x, "y":self.y})
+        self.test_obj.set_var_values(variables={"x": self.x, "y": self.y})
 
         # Analyze the system
         self.test_obj.analyze()
@@ -182,44 +192,63 @@ class TestAnalysisObject(unittest.TestCase):
         self.assertEqual(f_obj, f_exp, "Output values for f do not match")
 
         return
-    
+
     def test_isolated_adjoint(self):
         """
         Checks the computation of the isolated adjoint method for the analysis object
         """
 
         # Set the variable values for the analysis object
-        self.test_obj.set_var_values(variables={"x":self.x, "y":self.y})
+        self.test_obj.set_var_values(variables={"x": self.x, "y": self.y})
 
         # Declare both variables as design variables
         self.test_obj.declare_design_vars(variables=["x", "y"])
 
         # Perform the combined adjoint test for the object
-        rel_error = self.test_obj.test_isolated_adjoint(print_res=False, debug_print=False, method="cs", defined_vars={"test.x":2.0})
+        rel_error = self.test_obj.test_isolated_adjoint(
+            print_res=False,
+            debug_print=False,
+            method="cs",
+            defined_vars={"test.x": 2.0},
+        )
 
         # Check to make sure the relative error is less than 1e-6
-        self.assertLessEqual(rel_error, 1e-5, "Relative error for the isolated adjoint test is greater than 1e-5.")
+        self.assertLessEqual(
+            rel_error,
+            1e-5,
+            "Relative error for the isolated adjoint test is greater than 1e-5.",
+        )
 
-        return    
-        
+        return
+
     def test_combined_adjoint(self):
         """
         Checks the computation of the combined adjoint method for the analysis object
         """
 
         # Set the variable values for the analysis object
-        self.test_obj.set_var_values(variables={"x":self.x, "y":self.y})
+        self.test_obj.set_var_values(variables={"x": self.x, "y": self.y})
 
         # Declare both variables as design variables
         self.test_obj.declare_design_vars(variables=["x", "y"])
 
         # Perform the combined adjoint test for the object
-        rel_error = self.test_obj.test_combined_adjoint(print_res=False, debug_print=False, method="cs", defined_vars={"test.x":2.0})
+        rel_error = self.test_obj.test_combined_adjoint(
+            print_res=False,
+            debug_print=False,
+            method="cs",
+            defined_vars={"test.x": 2.0},
+        )
 
         # Check to make sure the relative error is less than 1e-6
-        self.assertLessEqual(rel_error, 1e-5, "Relative error for the combined adjoint test is greater than 1e-5.")
+        self.assertLessEqual(
+            rel_error,
+            1e-5,
+            "Relative error for the combined adjoint test is greater than 1e-5.",
+        )
 
-        return    
+        return
+
 
 if __name__ == "__main__":
     # Perform the unittests
