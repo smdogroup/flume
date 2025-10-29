23663c214b44174cd4d8d0a73050d0e67d17159d
csmith763 <csmith763@gatech.edu>
2025-10-29 11:17:32 -0400

    Visual updates for documentation site

--- docs/conf.py
+++ docs/conf.py
@@ -38,4 +38,6 @@ source_suffix = {
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
+html_static_path = ["source/_static"]
+html_css_files = ["custom.css"]
+html_js_files = ["repo.js"]
